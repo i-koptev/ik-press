239d8a0241478d5eb21a0a86060457f090d002ba
Igor Koptev <igor.koptef@gmail.com>
2020-07-31 14:15:14 +0300

    gatby-config.js gatsby-plugin-wp settings add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+let activeEnv =
+    process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+    path: `.env.${activeEnv}`,
+})
+
+console.log(`This WordPress Endpoint is used: '${process.env.WORDPRESS_URL}'`)
 
 module.exports = {
-    /* Your site config here */
-    plugins: [],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-wordpress`,
+            options: {
+                url: `${process.env.WORDPRESS_URL}/graphql`,
+                verbose: true,
+                develop: {
+                    nodeUpdateInterval: 1000,
+                    hardCacheMediaFiles: true,
+                },
+                html: {
+                    imageMaxWidth: 2048,
+                    fallbackImageMaxWidth: 800,
+                    imageQuality: 100,
+                },
+                type: {
+                    Post: {
+                        perPage: 5, // default is 10
+                        sortFields: `slug`, // default is date
+                        sortOrder: `ASC`, // default is DESC
+                    },
+                },
+                reports: {
+                    templateRouting: true,
+                },
+            },
+        },
+    ],
 }
