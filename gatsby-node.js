049c7ba5ac4f76dacb21816d5090740969e0911f
Igor Koptev <igor.koptef@gmail.com>
2020-07-31 17:50:52 +0300

    temporary notes

---
+++ gatsby-node.js
@@ -0,0 +1,26 @@
+// from https://github.com/henrikwirth/gatsby-starter-wordpress-twenty-twenty/blob/master/gatsby-node.js
+// mostly ...
+
+// const path = require("path")
+// const { dd } = require("dumper.js")
+// const { dump } = require("dumper.js")
+// const glob = require("glob")
+
+// dd(users)
+
+// const getTemplates = () => {
+//     const sitePath = path.resolve(`./`)
+//     return glob.sync(`./src/templates/**/*.js`, { cwd: sitePath })
+// }
+// // dd(getTemplates())
+
+// const createPages = require("./create/createPages")
+// const createPosts = require("./create/createPosts")
+
+// exports.createPagesStatefully = async (
+//     { graphql, actions, reporter },
+//     options
+// ) => {
+//     await createPages({ actions, graphql, reporter }, options)
+//     await createPosts({ actions, graphql, reporter }, options)
+// }
