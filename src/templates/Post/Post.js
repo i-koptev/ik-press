988692896634035b823f25dd1e9fc0010733b8ef
Igor Koptev <igor.koptef@gmail.com>
2020-08-10 15:51:25 +0300

    Lang switcher test

--- src/templates/Post/Post.js
+++ src/templates/Post/Post.js
@@ -1,8 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 
-const Post = ({ pageContext, data }) => {
+const Post = ({ pageContext, data, location }) => {
+    let l = location.pathname
+    let prefixedWithLocale = l.match(/^\/ru/) || l.match(/^\/en/)
+    const { languages, originalPath } = useI18next()
+
     return (
         <>
+            <Link to={!prefixedWithLocale ? "/en" + l : l.replace("ru", "en")}>
+                EN
+            </Link>
+            <br />
+            <Link to={!prefixedWithLocale ? "/ru" + l : l.replace("en", "ru")}>
+                RU
+            </Link>
+            <pre>{JSON.stringify(originalPath, null, 2)}</pre>
+
             <div
                 style={{
                     fontSize: "10px",
@@ -13,6 +27,11 @@ const Post = ({ pageContext, data }) => {
             >
                 <pre>
                     <b>Template:</b> {__filename}
+                </pre>
+                <pre>
+                    <b>Page location:</b>
+                    <br />
+                    {JSON.stringify(location, null, 2)}
                 </pre>
                 <pre>
                     <b>Page context:</b>
