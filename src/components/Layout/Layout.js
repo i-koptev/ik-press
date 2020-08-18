019738b9757a22b97fd793f58ca055bdec4aa484
Igor Koptev <igor.koptef@gmail.com>
2020-08-18 12:30:25 +0300

    Layout tuned

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,20 @@
 import React from "react"
-import { makeStyles, useTheme, createMuiTheme } from "@material-ui/core/styles"
-import Container from "@material-ui/core/Container"
-import Grid from "@material-ui/core/Grid"
 
 const Layout = ({ children }) => {
-    // const classes = useStyles()
-    const theme = useTheme()
-
     return (
-        <Container
-            //  defined in pages/blog/index.js
-            maxWidth={theme.siteContainer.maxWidth}
+        <div
+            style={{
+                // backgroundImage: "url(/img/bg_16.jpg)",
+                // backgroundRepeat: "repeat",
+                minHeight: "100vh",
+                height: "100%",
+            }}
         >
+            {/* <SEO title={title} description={description} lang={lang} /> */}
+            {/* <Navigation /> */}
             {children}
-        </Container>
+            {/* <Footer /> */}
+        </div>
     )
 }
 
